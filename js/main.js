25a78a6ee8a43807bacb3ed39c4bb20d1c030dad
InhaKr <inkr3891@gmail.com>
2022-05-21 19:04:27 +0300

    ДЗ 10. #13_1

--- js/main.js
+++ js/main.js
@@ -1,60 +1,124 @@
-// "use strict";
+"use strict";
 
-// ДЗ 13. #16_2
-// Написать скрипт который в title
-// страницы(document.title) будет
-// выводить текущее время и количество
-// секунд которое прошло после открытия
-// страниџы.
-// Сгенерировать при помощи JS элемент
-//  div с двумя span, где выводится 
-//  тоже самое время, что и в title 
+// ДЗ 10. #13_1
+// 1.Создать класс new SuperArray(n, m, { min: 10, max: 55 }),
+// который создаст массив размерностью n на m и заполнит
+// случайными числами в диапазоне options.min - options.max.
+// Массив сохраняется в екземпляр класса SuperArray.
+// 2.Создать метод render(separator), в прототипе. Который выведет
+// двумерный массив в документ, с разделителем separator.
+// 3.Создать метод clear(direction, k), где direction может быть 
+// "row" или "column", а k - номер строки или столбца, 
+// который нужно очистить. (поставить 0)
 
-//  в разные span соответственно
+// --------------задачи 1,2,3----------------------------
 
+function createMasiv(n, m) {
+  let a = new Array(n);
+  for (let i = 0; i < a.length; i++) {
+    a[i] = new Array(m);
 
-
-let l = setInterval(function clock() {
-  let d = new Date();
-
-  let hours = d.getHours();
-  let minutes = d.getMinutes();
-  let seconds = d.getSeconds();
-  let time = performance.now();
-
-  document.querySelector(".doc_time").innerHTML = (hours < 10 ? '0' : '') + hours + ":" + (minutes < 10 ? '0' : '') + minutes +  ":" + (seconds < 10 ? '0' : '') + seconds +'   //   '+ time;
-  document.querySelector(".span1").innerHTML = (hours < 10 ? '0' : '') + hours + ":" + (minutes < 10 ? '0' : '') + minutes +  ":" + (seconds < 10 ? '0' : '') + seconds +'   //   '+ time;
-  document.querySelector(".span2").innerHTML = (hours < 10 ? '0' : '') + hours + ":" + (minutes < 10 ? '0' : '') + minutes +  ":" + (seconds < 10 ? '0' : '') + seconds +'   //   '+ time;
-},1000);
+    for (let j = 0; j < a[i].length; j++) {
+      a[i][j] = Math.floor(Math.random() * 46) + 10;
+    }
+  }
+  return a
+}
 
 
+class SuperArray {
 
-let x = document.createElement('div');
-x.classList.add('myDiv');
-x.innerHTML = 'myDiv';
-x.style.backgroundColor= 'blue';
- 
+  constructor(p1) {
+    this.p1 = p1;
+  };
 
-let container = document.querySelector('.container');
-container.appendChild(x);
+  // createMasiv(n, m) {
+  //   let a = new Array(n);
+  //   for (let i = 0; i < a.length; i++) {
+  //     a[i] = new Array(m);
 
-let y = document.createElement('span');
-y.classList.add('span1');
-y.innerHTML = 'time';
-y.style.backgroundColor= 'yellow';
- 
+  //     for (let j = 0; j < a[i].length; j++) {
+  //       a[i][j] = Math.floor(Math.random() * 46) + 10;
+  //     }
+  //   }
+  //   return a
+  // }
 
-let r = document.querySelector('.myDiv');
-r.appendChild(y);
+  // render(n, m) {
+  //   let out = '';
+  //   let a = new Array(n);
+  //   for (let i = 0; i < a.length; i++) {
+  //     a[i] = new Array(m);
 
-let z = document.createElement('span');
-z.classList.add('span2');
-z.innerHTML = l;
-z.style.backgroundColor= 'orange';
- 
+  //     for (let j = 0; j < a[i].length; j++) {
+  //       a[i][j] = Math.floor(Math.random() * 46) + 10;
+  //       out += a[i][j] + ', ';
+  //       // console.log(out)
+  //     }
+  //     out += '<br>'
+  //     document.querySelector('.container').innerHTML = out;
+  //   }
+  //   return a
+  // }
 
-let s = document.querySelector('.myDiv');
-s.appendChild(z);
+  clear(direction, k) {
+    // debugger
+    let n = 3;
+    let m = 3;
+
+    let a = new Array(n);
+
+    for (let i = 0; i < a.length; i++) {
+      a[i] = new Array(m);
+
+      for (let j = 0; j < a[i].length; j++) {
+        a[i][j] = Math.floor(Math.random() * 41) - 20;
+      }
+    }
+
+    for (let i = 0; i < a.length; i++) {
+      // console.log(a[i])
+
+      if (direction === 'column') {
+        a[i][k] = 0;
+      }
+
+      for (let j = 0; j < a[i].length; j++) {
+        // console.log(a[j][i])
+        if (direction === 'row') {
+          a[k][j] = '*';
+        }
+      }
+
+    }
+    return (a);
+  }
+}
+
+let r = new SuperArray(createMasiv(4, 5));
+console.log(r);
+
+SuperArray.prototype.render = function (n, m) {
+  let out = '';
+  let a = new Array(n);
+  for (let i = 0; i < a.length; i++) {
+    a[i] = new Array(m);
+
+    for (let j = 0; j < a[i].length; j++) {
+      a[i][j] = Math.floor(Math.random() * 46) + 10;
+      out += a[i][j] + ', ';
+      // console.log(out)
+    }
+    out += '<br>'
+    document.querySelector('.container').innerHTML = out;
+  }
+  return a
+}
+
+console.log(r.render(5, 7))
+console.log(r.clear('column', 1))
+console.log(r.clear('row', 0))
+
 
 
 
